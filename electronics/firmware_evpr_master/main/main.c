54012da27909d90ef255edc9e99b1bcfa6441b62
petermccloud <peter@mccloudaero.com>
2017-07-09 17:33:26 -0700

    added basic functionality for master node status page via mongoose

--- electronics/firmware_evpr_master/main/main.c
+++ electronics/firmware_evpr_master/main/main.c
@@ -11,13 +11,125 @@
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
 
-#define WIFI_SSID "Goliath" 
+//#include "webserver.h"
+#include "mongoose.h"
 
+#define WIFI_SSID "Goliath" 
 #define BLINK_GPIO 5 
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     return ESP_OK;
+}
+
+char *mongoose_eventToString(int ev) {
+	static char temp[100];
+	switch (ev) {
+	case MG_EV_CONNECT:
+		return "MG_EV_CONNECT";
+	case MG_EV_ACCEPT:
+		return "MG_EV_ACCEPT";
+	case MG_EV_CLOSE:
+		return "MG_EV_CLOSE";
+	case MG_EV_SEND:
+		return "MG_EV_SEND";
+	case MG_EV_RECV:
+		return "MG_EV_RECV";
+	case MG_EV_HTTP_REQUEST:
+		return "MG_EV_HTTP_REQUEST";
+	case MG_EV_HTTP_REPLY:
+		return "MG_EV_HTTP_REPLY";
+	case MG_EV_MQTT_CONNACK:
+		return "MG_EV_MQTT_CONNACK";
+	case MG_EV_MQTT_CONNACK_ACCEPTED:
+		return "MG_EV_MQTT_CONNACK";
+	case MG_EV_MQTT_CONNECT:
+		return "MG_EV_MQTT_CONNECT";
+	case MG_EV_MQTT_DISCONNECT:
+		return "MG_EV_MQTT_DISCONNECT";
+	case MG_EV_MQTT_PINGREQ:
+		return "MG_EV_MQTT_PINGREQ";
+	case MG_EV_MQTT_PINGRESP:
+		return "MG_EV_MQTT_PINGRESP";
+	case MG_EV_MQTT_PUBACK:
+		return "MG_EV_MQTT_PUBACK";
+	case MG_EV_MQTT_PUBCOMP:
+		return "MG_EV_MQTT_PUBCOMP";
+	case MG_EV_MQTT_PUBLISH:
+		return "MG_EV_MQTT_PUBLISH";
+	case MG_EV_MQTT_PUBREC:
+		return "MG_EV_MQTT_PUBREC";
+	case MG_EV_MQTT_PUBREL:
+		return "MG_EV_MQTT_PUBREL";
+	case MG_EV_MQTT_SUBACK:
+		return "MG_EV_MQTT_SUBACK";
+	case MG_EV_MQTT_SUBSCRIBE:
+		return "MG_EV_MQTT_SUBSCRIBE";
+	case MG_EV_MQTT_UNSUBACK:
+		return "MG_EV_MQTT_UNSUBACK";
+	case MG_EV_MQTT_UNSUBSCRIBE:
+		return "MG_EV_MQTT_UNSUBSCRIBE";
+	case MG_EV_WEBSOCKET_HANDSHAKE_REQUEST:
+		return "MG_EV_WEBSOCKET_HANDSHAKE_REQUEST";
+	case MG_EV_WEBSOCKET_HANDSHAKE_DONE:
+		return "MG_EV_WEBSOCKET_HANDSHAKE_DONE";
+	case MG_EV_WEBSOCKET_FRAME:
+		return "MG_EV_WEBSOCKET_FRAME";
+	}
+	sprintf(temp, "Unknown event: %d", ev);
+	return temp;
+}
+
+char *mgStrToStr(struct mg_str mgStr) {
+	char *retStr = (char *) malloc(mgStr.len + 1);
+	memcpy(retStr, mgStr.p, mgStr.len);
+	retStr[mgStr.len] = 0;
+	return retStr;
+}
+
+void mongoose_event_handler(struct mg_connection *nc, int ev, void *evData) {
+	switch (ev) {
+	case MG_EV_HTTP_REQUEST: {
+			struct http_message *message = (struct http_message *) evData;
+
+			char *uri = mgStrToStr(message->uri);
+
+			if (strcmp(uri, "/status") == 0) {
+				char payload[256];
+				struct timeval tv;
+				gettimeofday(&tv, NULL);
+				sprintf(payload, "EVPR MASTER NODE STATUS\nTime since start: %d.%d secs", (int)tv.tv_sec, (int)tv.tv_usec);
+				int length = strlen(payload);
+				mg_send_head(nc, 200, length, "Content-Type: text/plain");
+				mg_printf(nc, "%s", payload);
+			}	else {
+				mg_send_head(nc, 404, 0, "Content-Type: text/plain");
+			}
+			nc->flags |= MG_F_SEND_AND_CLOSE;
+			free(uri);
+			break;
+		}
+	} // End of switch
+}
+
+void mongooseTask(void *data) {
+	printf("Mongoose task starting\n");
+	struct mg_mgr mgr;
+	printf("Mongoose: Starting setup\n");
+	mg_mgr_init(&mgr, NULL);
+	printf("Mongoose: Succesfully inited\n");
+	struct mg_connection *c = mg_bind(&mgr, ":80", mongoose_event_handler);
+	printf("Mongoose Successfully bound\n");
+	if (c == NULL) {
+		printf("No connection from the mg_bind()\n");
+		vTaskDelete(NULL);
+		return;
+	}
+	mg_set_protocol_http_websocket(c);
+
+	while (1) {
+		mg_mgr_poll(&mgr, 1000);
+	}
 }
 
 static void initialise_wifi(void)
@@ -40,8 +152,14 @@ static void initialise_wifi(void)
     ESP_ERROR_CHECK( esp_wifi_start() );
 }
 
+//    wifi_sta_list_t station_list;
+//    ESP_ERROR_CHECK( esp_wifi_ap_get_sta_list(&station_list) );
+//    printf(station_list);
+
 void blink_task(void *pvParameter)
 {
+    
+
     /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
        muxed to GPIO on reset already, but some default to other
        functions and need to be switched to GPIO. Consult the
@@ -58,6 +176,7 @@ void blink_task(void *pvParameter)
         /* Blink on (output high) */
         gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
+        
     }
 }
 
@@ -68,4 +187,5 @@ void app_main()
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
     initialise_wifi();
     xTaskCreate(&blink_task, "blink_task", 2048, NULL, 5, NULL);
+    xTaskCreatePinnedToCore(&mongooseTask, "mongooseTask", 20000, NULL, 5, NULL,0);
 }
