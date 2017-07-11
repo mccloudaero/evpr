2540dd35315555837340f1fb6b984a7d2478aee4
petermccloud <peter@mccloudaero.com>
2017-07-10 19:49:17 -0700

    added position hold mode to slave

--- electronics/firmware_evpr_slave/main/main.c
+++ electronics/firmware_evpr_slave/main/main.c
@@ -18,12 +18,14 @@
 
 #define WIFI_SSID "Goliath" 
 
+#define MODE 1 // Options 0=self_test, 1=position_hold
+
 //You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
 #define SERVO_MIN_PULSEWIDTH 1000 //Minimum pulse width in microsecond
 #define SERVO_MAX_PULSEWIDTH 2000 //Maximum pulse width in microsecond
 #define SERVO_MAX_DEGREE 90 //Maximum angle in degree upto which servo can rotate
 
-static void mcpwm_example_gpio_initialize()
+static void mcpwm_gpio_initialize()
 {
     printf("initializing mcpwm servo control gpio......\n");
     mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, 16);    //Set GPIO 16 as PWM0A, to which servo is connected
@@ -45,14 +47,11 @@ static uint32_t servo_per_degree_init(uint32_t degree_of_rotation)
     return cal_pulsewidth;
 }
 
-/**
- * @brief Configure MCPWM module
- */
-void mcpwm_example_servo_control(void *arg)
+void servo_self_test(void *arg)
 {
     uint32_t angle, count;
     //1. mcpwm gpio initialization
-    mcpwm_example_gpio_initialize();
+    mcpwm_gpio_initialize();
 
     //2. initial mcpwm configuration
     printf("Configuring Initial Parameters of mcpwm......\n");
@@ -73,6 +72,34 @@ void mcpwm_example_servo_control(void *arg)
             mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, angle);
             vTaskDelay(10);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
         }
+    }
+}
+
+void position_hold(void *arg)
+{
+    uint32_t angle, count;
+    //1. mcpwm gpio initialization
+    mcpwm_gpio_initialize();
+
+    //2. initial mcpwm configuration
+    printf("Configuring Initial Parameters of mcpwm......\n");
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+
+    //3. Hold position at specified angle
+    count = 0;
+    printf("Angle of rotation: %d\n", count);
+    angle = servo_per_degree_init(count);
+    printf("pulse width: %dus\n", angle);
+    while (1) {
+        mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, angle);
+        mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, angle);
     }
 }
 
@@ -100,6 +127,12 @@ void app_main()
     nvs_flash_init();
     tcpip_adapter_init();
     initialise_wifi();
-    printf("Testing servo motor.......\n");
-    xTaskCreate(mcpwm_example_servo_control, "mcpwm_example_servo_control", 4096, NULL, 5, NULL);
+    switch (MODE) {
+    case 0:
+      printf("Self Test Mode\n");
+      xTaskCreate(servo_self_test, "servo_self_test", 4096, NULL, 5, NULL);
+    case 1:
+      printf("Position Hold Mode\n");
+      xTaskCreate(position_hold, "position_hold", 4096, NULL, 5, NULL);
+    }
 }
