5eaaccfe621380e4ce9c8c33c715145322ce04f9
petermccloud <peter@mccloudaero.com>
2018-01-20 17:32:21 -0800

    renamed espnow vars in slave firmware

--- electronics/firmware_evpr_slave/main/espnow.h
+++ electronics/firmware_evpr_slave/main/espnow.h
@@ -7,34 +7,34 @@
 
 #define ESPNOW_QUEUE_SIZE 6
 
-#define IS_BROADCAST_ADDR(addr) (memcmp(addr, example_broadcast_mac, ESP_NOW_ETH_ALEN) == 0)
+#define IS_BROADCAST_ADDR(addr) (memcmp(addr, espnow_broadcast_mac, ESP_NOW_ETH_ALEN) == 0)
 
 typedef enum {
     EXAMPLE_ESPNOW_SEND_CB,
     EXAMPLE_ESPNOW_RECV_CB,
-} example_espnow_event_id_t;
+} espnow_event_id_t;
 
 typedef struct {
     uint8_t mac_addr[ESP_NOW_ETH_ALEN];
     esp_now_send_status_t status;
-} example_espnow_event_send_cb_t;
+} espnow_event_send_cb_t;
 
 typedef struct {
     uint8_t mac_addr[ESP_NOW_ETH_ALEN];
     uint8_t *data;
     int data_len;
-} example_espnow_event_recv_cb_t;
+} espnow_event_recv_cb_t;
 
 typedef union {
-    example_espnow_event_send_cb_t send_cb;
-    example_espnow_event_recv_cb_t recv_cb;
-} example_espnow_event_info_t;
+    espnow_event_send_cb_t send_cb;
+    espnow_event_recv_cb_t recv_cb;
+} espnow_event_info_t;
 
 /* When ESPNOW sending or receiving callback function is called, post event to ESPNOW task. */
 typedef struct {
-    example_espnow_event_id_t id;
-    example_espnow_event_info_t info;
-} example_espnow_event_t;
+    espnow_event_id_t id;
+    espnow_event_info_t info;
+} espnow_event_t;
 
 enum {
     EXAMPLE_ESPNOW_DATA_BROADCAST,
@@ -50,7 +50,7 @@ typedef struct {
     uint16_t crc;                         //CRC16 value of ESPNOW data.
     uint32_t magic;                       //Magic number which is used to determine which device to send unicast ESPNOW data.
     uint8_t payload[0];                   //Real payload of ESPNOW data.
-} __attribute__((packed)) example_espnow_data_t;
+} __attribute__((packed)) espnow_data_t;
 
 
 /* Parameters of sending ESPNOW data. */
@@ -64,6 +64,6 @@ typedef struct {
     int len;                              //Length of ESPNOW data to be sent, unit: byte.
     uint8_t *buffer;                      //Buffer pointing to ESPNOW data.
     uint8_t dest_mac[ESP_NOW_ETH_ALEN];   //MAC address of destination device.
-} example_espnow_send_param_t;
+} espnow_send_param_t;
 
 #endif
