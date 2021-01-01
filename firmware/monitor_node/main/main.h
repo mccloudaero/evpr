cc2a6de55a4afa02b203bf3a0c5d4cabc6fadd07
Peter <peter@mccloudaero.com>
2020-12-31 20:04:31 -0800

    firmware, monitor_node - cleaned up header file

--- firmware/monitor_node/main/main.h
+++ firmware/monitor_node/main/main.h
@@ -1,19 +1,8 @@
-#define TAG "evpr_master"
+#define TAG "evpr_monitor_node"
 #define WIFI_SSID CONFIG_WIFI_SSID
 #define WIFI_PWD CONFIG_WIFI_PASSWORD
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 #define UDP_PKTSIZE CONFIG_UDP_PKT_SIZE
-
-// IPs and ports
-#define MASTER_IP "192.168.4.1"
-#define ROTOR_1_IP "192.168.4.11" 
-#define ROTOR_1_PORT 6001 
-#define ROTOR_2_IP "192.168.4.12" 
-#define ROTOR_2_PORT 6002 
-#define ROTOR_3_IP "192.168.4.13" 
-#define ROTOR_3_PORT 6003 
-#define ROTOR_4_IP "192.168.4.14" 
-#define ROTOR_4_PORT 6004 
 
 //extern EventGroupHandle_t comm_event_group;
 extern bool message_received;
